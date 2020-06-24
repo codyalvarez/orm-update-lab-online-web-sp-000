8fe25fcb649a25cf6bee3349054cfb9022157c23
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-24 23:59:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,10 @@ class Student
     student
   end
 
+  def self.new_from_db
+
+  end
+
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
