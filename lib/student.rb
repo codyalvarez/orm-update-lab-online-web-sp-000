bff02aa21246a3db95197a0b8ec291bf8cb97969
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-25 00:06:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,16 @@ class Student
   def self.new_from_db(arg)
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def update
