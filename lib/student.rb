3e18735a0fee86b9c85c829c7d29adc0fae0f458
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-25 00:09:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,6 +48,12 @@ class Student
   end
 
   def self.new_from_db(arg)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+
   end
 
   def self.find_by_name(name)
